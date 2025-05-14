94c7440ea9cbc947062f2dbdd6464e61a6ef0755
Ferni0906 <lfernandomtr@gmail.com>
2025-05-13 21:16:25 -0600

    .

--- semana2/clase1_tarea.js
+++ semana2/clase1_tarea.js
@@ -3,7 +3,12 @@
 const { ask } = require('../helpers/input');
 
 async function main() {
-    
+    const pares = Number (await ask("Ingresa los numeros pares del 1 al 100:"));
+
+    for ( i = 1; i <= 100; i++)
+        if (i % 2 === 0) {
+            console.log(i);
+        }
 }
 
 main();
