86e9b6b43fcf48abff53cc7fb2e1a4c5c7789097
Ferni0906 <lfernandomtr@gmail.com>
2025-05-19 21:11:41 -0600

    .

--- semana2/clase2_tarea.js
+++ semana2/clase2_tarea.js
@@ -11,3 +11,31 @@
 // 2. Eliminar fruta
 // 3. Ver lista de frutas
 // 4. Salir
+
+
+const { ask } = require('../helpers/input');
+
+async function main(){
+
+    let opcion = '';
+    let frutas = [];
+
+    while (opcion !== '4'){
+        opcion = await ask('¿Que deseas hacer? \n1. Agregar fruta \n2. Eliminar fruta \n3. Ver lista de frutas \n4. Salir');
+        if (opcion === '1'){
+            //Agregar
+            let frutaNueva= await ask('¿Que fruta deseas agregar?: ');
+            frutas.push(frutaNueva);
+            console.log(`Fruta agregada: ${frutaNueva}`);
+            console.log('Frutas disponibles: ', frutas);
+        }
+
+    }
+
+
+
+
+
+}
+
+main();
--- semana2/clase4_tarea.js
+++ semana2/clase4_tarea.js
@@ -12,3 +12,55 @@
 //Promedio general
 //
 //La calificación más alta y la más baja
+
+function analizarCalificaciones(calificaciones) {
+    let aprobados= obtenerAprobados(calificaciones);
+    let reprobados= 0;
+    let promedio= obtenerPromedio(calificaciones);
+    let califAlta=obtenerMayor(calificaciones);
+    let califBaja=obtenerMenor(calificaciones);
+
+    return{ aprobados, reprobados, promedio, califAlta, califBaja }
+}
+
+function obtenerPromedio(numeros) {
+  let total = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    total = total + numeros[i];
+  }
+  const promedio = total / numeros.length;
+
+  return promedio;
+}
+
+function obtenerMayor(numeros) {
+  let mayor = numeros[0];
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] > mayor) {
+      mayor = numeros[i];
+    }
+  }
+  return mayor;
+}
+
+function obtenerMenor(numeros) {
+  let menor = numeros[0];
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] < menor) {
+      menor = numeros[i];
+    }
+  }
+  return menor;
+}
+
+function obtenerAprobados(numeros){
+    let aprobados= 0;
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] >= 70) {
+            aprobados++;
+        }else {
+            reprobados++;
+        }
+    }
+    return aprobados;
+}
